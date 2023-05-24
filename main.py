0b2e89d7d339e606c0c7cb97361dc64c7b03208d
thevitorhideki <7vitorgomez@gmail.com>
2023-05-24 09:32:50 -0300

    Comments and code refactor

--- main.py
+++ main.py
@@ -30,33 +30,39 @@ class Player(pygame.sprite.Sprite):
         self.jump_bool = False
 
     def isCollidingPlatform(self, platforms):
+        # Checks if the player is colliding with a platform
         platforms_hit = pygame.sprite.spritecollide(self, platforms, False)
         for platform in platforms_hit: 
             if self.rect.colliderect(platform.rect): 
+                # Returns witch platform the player is colliding with
                 return platform
         return False
     
     def isCollidingGround(self, ground):
+        # Checks if the player is colliding with the ground
         for g in ground:
             if self.rect.colliderect(g.rect):
                 return True
         return False
     
     def jump(self):
-        # Verify if player press space and if it is, jump
+        # Verify if player press space and he's not jumping if both are true, jump
         if keys[pygame.K_SPACE] and self.jump_bool == False:
             self.speedy -= 20
             self.jump_bool = True
     
     def apply_gravity(self):
-        # If Player is jumping and the space key is pressed, apply gravity to the player.
+        # If Player is in the air and the space key is pressed, apply the glide effect.
         if keys[pygame.K_SPACE] and self.speedy >= 0:
             self.gravity = 0.1
         else:
             self.gravity = 1
+            
+        # Gravity effect
         self.speedy += self.gravity
         self.rect.y += self.speedy
         if self.isCollidingGround(ground):
+            # Checks if the player is colliding with the ground
             for g in ground:
                 if self.rect.bottom > g.rect.top + 1:
                     self.rect.bottom = g.rect.top + 1
@@ -66,93 +72,90 @@ class Player(pygame.sprite.Sprite):
         
         if self.isCollidingPlatform(platforms):
             platform = self.isCollidingPlatform(platforms)
+            # Checks if the player is colliding with a platform and if he is, checks if he is falling or jumping
             if self.speedy > 0:
+                # If player is falling, stop him from falling and set his position to the top of the platform
                 self.rect.bottom = platform.rect.top + 1
                 self.jump_bool = False
                 self.speedy = 0
-                
             elif self.speedy < 0:
+                # If player is jumping, stop him from jumping and set his position to the bottom of the platform
                 self.rect.top = platform.rect.bottom 
                 self.speedy = 0
         
     
     def animation_state(self):
-        # If player is colliding with platform or ground, change sprite to walking animation
-        if self.isCollidingPlatform(platforms) or self.isCollidingGround(ground):
+        # If player is not in the air, play the walking animation
+        if self.speedy == 0:
             self.player_index += 0.1
             self.image = self.player_walk[int(self.player_index % len(self.player_walk))]
         # else:
         #     self.image = self.player_jump
     
+    def collision_player_rocks():
+        # Checks if the player is colliding with a rock
+        if pygame.sprite.spritecollide(player.sprite, rocks, False):
+            # If player is colliding with a rock, clear the list of rocks and return False
+            rocks.empty()
+            return False
+        else:
+            return True
+        
     def update(self):
         self.apply_gravity()
         self.jump()
         self.animation_state()
 
-
-def collision_player_rocks():
-    if pygame.sprite.spritecollide(player.sprite, rocks, False):
-        rocks.empty()
-        return False
-    else:
-        return True
-
-
+# Window settings
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Evolution Run")
+
 clock = pygame.time.Clock()
 font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-playing = True
-
-# Player
+# Player Group
 player = pygame.sprite.GroupSingle()
 player.add(Player())
 
 ground.add(Ground())
 
-rocks_list = []
-
 # Timers
 platform_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(platform_timer, 2000)
+
 rocks_timer = pygame.USEREVENT + 2
 pygame.time.set_timer(rocks_timer, 1800)
-clock = pygame.time.Clock()
+
+playing = True
 
 # Pontuação
 score = 0
 score_text = font.render("Score: " + str(score), True, (255, 255, 255))
 score_rect = score_text.get_rect(topright=(WIDTH - 660, 10))
 
-
-
-
-
 while True:
+    # Get a tuple with all the keys, if the key is pressed, the value is True, if not, False
     keys = pygame.key.get_pressed()
+    
     for event in pygame.event.get():
+        # Check if the player clicks the X button
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # Platform and rock timers
         if event.type == platform_timer:
             platforms.add(Platforms())
         if event.type == rocks_timer:
             rocks.add(Rocks())
-    
-    # Infinite background logic
+
+    # Fill the background with a color
     screen.fill((146, 244, 255))
+    # Place the score text on the screen
     screen.blit(score_text, score_rect)
 
-    # if background2_rect.topright[0] == WIDTH:
-    #     background_rect.x = WIDTH
-    # elif background_rect.topright[0] == WIDTH:
-    #     background2_rect.x = WIDTH
-
-    # background_rect.x -= 4
-    # background2_rect.x -= 4
-
+    # Check if the game is running
     if playing:
+        # Draw the player, platforms, rocks and ground on the screen
         player.draw(screen)
         player.update()
         platforms.draw(screen)
@@ -160,14 +163,27 @@ while True:
         rocks.draw(screen)
         rocks.update()
         ground.draw(screen)
-        playing = collision_player_rocks()
+        # Check if the player collides with a rock, if he does, stop the game
+        playing = Player.collision_player_rocks()
+        # Update the score
+        score += 0.2
         score_text = font.render("Score: " + str(floor(score)), True, (255, 255, 255))
+    # If the game is not running, show the game over screen
     else:
+        # Clear all the sprites
+        player.empty()
+        rocks.empty()
         platforms.empty()
+        
+        # Fill the screen with a color
         screen.fill((94,129,162))
+        
+        # Show the game over text and the score
         game_over_text = font.render("Game Over", True, (255, 255, 255))
         game_over_rect = game_over_text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
         screen.blit(game_over_text, game_over_rect)
+        screen.blit(score_text, score_rect)
+        # If the player press space, restart the game
         if keys[pygame.K_SPACE]:
             playing = True
             score = 0
@@ -176,7 +192,5 @@ while True:
             rocks.empty()
             platforms.empty()
         
-
     pygame.display.update()
     clock.tick(60)
-    score+=0.2
