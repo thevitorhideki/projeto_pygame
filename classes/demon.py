29b30feac807e63dccef2322bd82cacdc2961634
Enzo Junqueira <enzopj@al.insper.edu.br>
2023-05-30 14:55:58 -0300

    demon pastas

--- classes/demon.py
+++ classes/demon.py
@@ -3,7 +3,7 @@ import pygame
 class Demon(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.demon_list = [pygame.image.load(f'assets/demon{i}.png').convert_alpha() for i in range(1, 9)]
+        self.demon_list = [pygame.image.load(f'assets/demon/demon{i}.png').convert_alpha() for i in range(1, 9)]
         self.demon_index = 0
         self.image = self.demon_list[self.demon_index]
         self.rect = self.image.get_rect(bottomleft=(1280 * 3, 620))
