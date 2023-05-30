e7c2a71a31cba873bce61f734a2428b3ddde27ee
Enzo Junqueira <enzopj@al.insper.edu.br>
2023-05-30 14:44:05 -0300

    portal, rocks and tree (documentação)

--- classes/rocks.py
+++ classes/rocks.py
@@ -1,24 +1,49 @@
 import pygame
 
 class Rocks(pygame.sprite.Sprite):
+    """Classe que representa as rochas no jogo."""
+
     def __init__(self, x_pos, y_pos, style):
+        """
+        Inicializa um objeto Rock.
+
+        Carrega a imagem da rocha e define as posições iniciais.
+
+        Args:
+            x_pos (int): Posição x inicial da rocha.
+            y_pos (int): Posição y inicial da rocha.
+            style (str): Caminho do arquivo de imagem da rocha.
+
+        """
         super().__init__()
         rock = pygame.image.load(style).convert_alpha()
         self.image = rock
         
-        # Positioning the platform sprite on a random y axis
-        self.rect = self.image.get_rect(bottomleft = (x_pos, y_pos))
+        # Posiciona a rocha na parte inferior esquerda com as coordenadas fornecidas
+        self.rect = self.image.get_rect(bottomleft=(x_pos, y_pos))
     
     def destroy(self):
-        # Destroy platform if it goes off screen
+        """
+        Destroi a rocha se ela sair da tela.
+
+        """
         if self.rect.x <= -300:
             self.kill()
 
     def movement(self):
-        # Move rock to the left
+        """
+        Move a rocha para a esquerda.
+
+        """
         self.rect.x -= 4
 
     def update(self):
+        """
+        Atualiza a rocha.
+
+        Aplica o movimento da rocha e verifica se precisa ser destruída.
+
+        """
         self.movement()
         self.destroy()
         
--- classes/tree.py
+++ classes/tree.py
@@ -1,23 +1,43 @@
 import pygame
 
 class Tree(pygame.sprite.Sprite):
+    """Classe que representa uma árvore no jogo."""
+
     def __init__(self):
+        """
+        Inicializa um objeto Tree.
+
+        Carrega a imagem da árvore, ajusta seu tamanho e define a posição inicial.
+
+        """
         super().__init__()
         tree = pygame.image.load('assets/tree.png').convert_alpha()
         tree = pygame.transform.scale(tree, (150, 150))
         self.image = tree
-        self.rect = self.image.get_rect(bottomleft = (0,620))
+        self.rect = self.image.get_rect(bottomleft=(0, 620))
     
     def destroy(self):
-        # Destroy platform if it goes off screen
+        """
+        Destroi a árvore se ela sair da tela.
+
+        """
         if self.rect.x <= -300:
             self.kill()
 
     def movement(self):
-        # Move rock to the left
+        """
+        Move a árvore para a esquerda.
+
+        """
         self.rect.x -= 4
 
     def update(self):
+        """
+        Atualiza a árvore.
+
+        Aplica o movimento da árvore e verifica se precisa ser destruída.
+
+        """
         self.movement()
         self.destroy()
 
