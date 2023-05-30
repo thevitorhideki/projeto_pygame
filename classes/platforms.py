db40c0166e9ce9942e01e456ab2bfd7d338fd2ed
thevitorhideki <7vitorgomez@gmail.com>
2023-05-30 14:55:03 -0300

    docstring da classe demon, ground e platforms

--- classes/platforms.py
+++ classes/platforms.py
@@ -1,26 +1,51 @@
 import pygame
 
 class Platforms(pygame.sprite.Sprite):
+    """
+    Classe que representa uma plataforma do jogo
+    """
+
     def __init__(self, x_pos, y_pos, style):
+        """
+        Construtor da classe Plataforma
+
+        Parâmetros:
+        - x_pos (int): Posição x inicial da plataforma.
+        - y_pos (int): Posição y inicial da plataforma.
+        - style (str): Caminho até o arquivo do sprite de plataforma.
+        """
         super().__init__()
-        # Load platform sprite
         city_platform = pygame.image.load(style).convert_alpha()
+        # Load the platform image with transparency
         self.image = city_platform
-        
-        # Positioning the platform sprite on a random y axis
-        self.rect = self.image.get_rect(bottomleft = (x_pos, y_pos))
-
+        # Set the image of the platform
+        self.rect = self.image.get_rect(bottomleft=(x_pos, y_pos))
+        # Create a rect object for the platform with its initial position
+    
     def destroy(self):
-        # Destroy platform if it goes off screen
+        """
+        Destrói a plataforma quando ela sai da tela.
+        """
         if self.rect.x <= -300:
+            # Check if the platform has gone off the left side of the screen
             self.kill()
-
+            # Remove the platform from the sprite group
+    
     def movement(self):
-        # Move platform to the left
+        """
+        Move a plataforma horizontalmente.
+        """
         self.rect.x -= 4
-
+        # Move the platform rect horizontally to the left by 4 pixels in each update
+    
     def update(self):
+        """
+        Atualiza o movimento da plataforma e a destrói se necessário.
+        """
         self.movement()
+        # Call the movement method to update the platform's position
         self.destroy()
-        
+        # Call the destroy method to check if the platform needs to be destroyed
+    
 platforms = pygame.sprite.Group()
+# Create a sprite group for the platforms
